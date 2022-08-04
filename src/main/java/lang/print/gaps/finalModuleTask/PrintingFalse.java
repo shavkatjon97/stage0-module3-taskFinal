23bf2606acdcb95159c403a01667761311b058bc
Shavkatjon97 <dadshavkatjon97@gmail.com>
2022-08-04 17:01:10 +0500

    Final was done

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
-        int second = -17;
-        //write code here
+        int second = -1;
+        System.out.println(first==second);
     }
 }
